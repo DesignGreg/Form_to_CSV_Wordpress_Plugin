d31ecc3422b3e6b98dafb2bda5d5d8c901cc26c5
Gregory <huyghegregory@yahoo.fr>
2018-10-16 11:51:21 +0200

    en cours

--- wp-content/plugins/form-to-csv/form-to-csv.php
+++ wp-content/plugins/form-to-csv/form-to-csv.php
@@ -8,7 +8,7 @@
 
 
 // 1. Code HTML du formulaire d'inscription
-function htmlCode () {
+function ftc_htmlCode () {
 ?> 
 <form action="" method="post">
     <label for="prenom">Prénom</label>
@@ -53,9 +53,34 @@ function htmlCode () {
  
 <?php   
  
-}    
+}
+
+
+// 2. Shortcode
+function ftc_shortcode() {
+    ftc_htmlCode();
+}
+
+add_shortcode( 'form_csv', 'ftc_shortcode' );
+
+
+// 3. Menu Admin pour voir données collectées
+function ftc_menu_item() {
+    add_menu_page(
+        __( 'Form to CSV', 'textdomain' ),
+        'Form to CSV',
+        'manage_options',
+        'custom_menu_page',
+        '',
+        'dashicons-portfolio',
+        21
+    );
+}
+
+add_action('admin_menu', 'ftc_menu_item');
+
     
-// 2. Récupérer ses infos dans un custom post accessible depuis le panneau admin. Pas d'envoi de mail. Envoi données vers DB
+// 4. Récupérer ses infos dans un custom post accessible depuis le panneau admin. Pas d'envoi de mail. Envoi données vers DB
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 $email = $_POST['email'];
@@ -74,79 +99,6 @@ function insertuser( $prenom, $nom, $email ) {
 insertuser( $prenom, $nom, $email );
 
 
-
-// 3. Pouvoir extraire ses infos dans un fichier .csv depuis le panneau admin. Requête DB
+// 5. Pouvoir extraire ses infos dans un fichier .csv depuis le panneau admin. Requête DB
     
-    
-    
-    
-    
-// THE FORM  
-function html_form_code() {
-    
-    echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
-    
-    echo '<p>';
-    echo 'Your Name (required) <br />';
-    echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    
-    echo '<p>';
-    echo 'Your Email (required) <br />';
-    echo '<input type="email" name="cf-email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    
-    echo '<p>';
-    echo 'Subject (required) <br />';
-    echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    
-    echo '<p>';
-    echo 'Your Message (required) <br />';
-    echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
-    echo '</p>';
-    
-    echo '<p><input type="submit" name="cf-submitted" value="Send"/></p>';
-    echo '</form>';
-}
-
-// SEND THE FORM DATA TO ADMIN EMAIL ADDRESS
-function deliver_mail() {
-
-    // if the submit button is clicked, send the email
-    if ( isset( $_POST['cf-submitted'] ) ) {
-
-        // sanitize form values
-        $name    = sanitize_text_field( $_POST["cf-name"] );
-        $email   = sanitize_email( $_POST["cf-email"] );
-        $subject = sanitize_text_field( $_POST["cf-subject"] );
-        $message = esc_textarea( $_POST["cf-message"] );
-
-        // get the blog administrator's email address
-        $to = get_option( 'admin_email' );
-
-        $headers = "From: $name <$email>" . "\r\n";
-
-        // If email has been process for sending, display a success message
-        if ( wp_mail( $to, $subject, $message, $headers ) ) {
-            echo '<div>';
-            echo '<p>Thanks for contacting me, expect a response soon.</p>';
-            echo '</div>';
-        } else {
-            echo 'An unexpected error occurred';
-        }
-    }
-}
-
-// SHORTCODE TO DISPLAY FORM ON WEBSITE
-function cf_shortcode() {
-    ob_start();
-    deliver_mail();
-    html_form_code();
-
-    return ob_get_clean();
-}
-
-add_shortcode( 'form_csv', 'cf_shortcode' );
-
-?>
+   
