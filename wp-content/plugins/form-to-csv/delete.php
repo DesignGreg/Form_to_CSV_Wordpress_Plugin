5741fe47830db2a68bd6bbcdaa6feb2a82a8e6a4
Gregory <huyghegregory@yahoo.fr>
2018-10-28 19:04:10 +0100

    -

--- wp-content/plugins/form-to-csv/delete.php
+++ wp-content/plugins/form-to-csv/delete.php
@@ -1,6 +1,6 @@
 <?php
-        if(isset($_GET['delete'])) {
-            $delete_file = $_GET['C:\Users\huygh\Desktop\form-to-csv.csv'];
-            unlink("$delete_file");
-        }
+    if(isset($_GET['delete'])) {
+        $delete_file = $_GET['C:\Users\huygh\Desktop\form-to-csv.csv'];
+        unlink("$delete_file");
+    }
 ?>
